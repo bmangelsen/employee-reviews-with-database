3ef73fe61d600c26d9ec90cad3ce2efddd3b7081
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-26 13:15:36 -0400

    Added all base assignment files, along with migration file to create database

---
+++ migration.rb
@@ -0,0 +1,32 @@
+require 'active_record'
+require 'sqlite3'
+
+ActiveRecord::Base.establish_connection({
+  adapter: 'sqlite3',
+  database: 'employee_reviews.sqlite3'
+  })
+
+class InitialMigration < ActiveRecord::Migration[5.0]
+
+  def change
+    create_table :department do |t|
+      t.string :name
+    end
+
+    create_table :employee do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.float :salary
+    end
+  end
+
+end
+
+
+begin
+  InitialMigration.migrate(:down)
+rescue
+end
+
+InitialMigration.migrate(:up)
