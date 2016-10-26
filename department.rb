08d76ca39c56aecdebc39193368acfbdfb2c82f3
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-26 16:02:47 -0400

    Added method to calculate total number of department employees, and created method to link department id with employee id

--- department.rb
+++ department.rb
@@ -7,15 +7,24 @@ class Department < ActiveRecord::Base
     new_employee.save
   end
 
+  def department_employees
+    Employee.where(department_id: self.id).to_a
+  end
+
   def department_salary
-    a = Employee.where(department_id: self.id).to_a
-    a.reduce(0.0) {|sum, e| sum + e.salary}
+    employees = department_employees
+    employees.reduce(0.0) {|sum, e| sum + e.salary}
   end
 
   def department_raise(alloted_amount)
-    a = Employee.where(department_id: self.id).to_a
-    raise_eligible = a.select {|e| yield(e)}
+    employees = department_employees
+    raise_eligible = employees.select {|e| yield(e)}
     amount = alloted_amount / raise_eligible.length
     raise_eligible.each {|e| e.raise_by_amount(amount)}
+  end
+
+  def total_department_employees
+    employees = department_employees
+    employees.count
   end
 end
